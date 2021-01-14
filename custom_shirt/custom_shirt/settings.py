f46f7925b64fda4f997bbf8935609401e731c07a
lion7472 <m.ghaffari662@gmail.com>
2021-01-14 21:22:39 +0330

    Add JWT

--- custom_shirt/custom_shirt/settings.py
+++ custom_shirt/custom_shirt/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import datetime
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,11 +57,40 @@ REST_FRAMEWORK = {
     ),
 }
 
-# JWT_AUTH = {
-#     'JWT_RESPONSE_PAYLOAD_HANDLER':
-#         'app.utils.jwt_response_payload_handler',
-#     'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=30000),
-# }
+JWT_AUTH = {
+    # 'JWT_RESPONSE_PAYLOAD_HANDLER':
+    #     'app.utils.jwt_response_payload_handler',
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=30000),
+    'JWT_ALLOW_REFRESH' : True,
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_AUTH_COOKIE': None,
+
+}
 
 
 MIDDLEWARE = [
--- custom_shirt/user/urls.py
+++ custom_shirt/user/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from rest_framework_jwt.views import obtain_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 from user import views
 
@@ -10,9 +9,10 @@ app_name='user'
 #...
 
 urlpatterns = [
+	path('api-token-auth/', obtain_jwt_token),
+	path('api-token-refresh/', refresh_jwt_token),
+	path('api-token-verify/', verify_jwt_token),
 	path('create/', views.CreateUserView.as_view(), name='create'),
 	path('token/', views.CreateTokenView.as_view(), name='token'),
 	path('me/', views.ManageUserView.as_view(), name='me'),
-	path('api-token-auth/', obtain_jwt_token),
-	path('api-token-refresh/', refresh_jwt_token)
 ]
